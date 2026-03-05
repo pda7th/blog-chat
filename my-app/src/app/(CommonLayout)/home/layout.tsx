f4d84f3a7640461eb4ffc03cb43ba0e889e444c0
안혜연 <ahy020606@naver.com>
2026-03-05 15:27:07 +0900

    etc: 채팅 홈레이아웃으로 이동

---
+++ my-app/src/app/(CommonLayout)/home/layout.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from 'react';
+import ChatSidebar from '@/components/chat/ChatSidebar';
+
+export default function HomeLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-[calc(100vh-70px)]">
+      <div className="flex-1 overflow-y-auto">{children}</div>
+      <Suspense>
+        <ChatSidebar />
+      </Suspense>
+    </div>
+  );
+}
--- my-app/src/app/(CommonLayout)/layout.tsx
+++ my-app/src/app/(CommonLayout)/layout.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from 'react';
 import NavBar from '@/components/common/NavBar/NavBar';
-import ChatSidebar from '@/components/chat/ChatSidebar';
 
 export default function CommonLayout({
   children,
@@ -14,9 +13,6 @@ export default function CommonLayout({
       </Suspense>
       <div className="children-container flex">
         <div className="flex-1 overflow-y-auto">{children}</div>
-        <Suspense>
-          <ChatSidebar />
-        </Suspense>
       </div>
     </>
   );
